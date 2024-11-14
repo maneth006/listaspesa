99005e1428d88dd1c9cc4e6b367c6849ba0cfab7
net <net@PC1-17.igalvani.local>
2024-11-14 09:50:33 +0100

    commit non finito

--- listaspesa.py
+++ listaspesa.py
@@ -4,7 +4,22 @@ def aggiunta():
     listaspesa.append(risp)
 
 def visualizza():
+    for i in range(len(listaspesa)):
+        print(f"{i}. {listaspesa[i]}")
+        i =  i + 1
+
+def elimina():
+    risp1 = int(input("Dimmi l'indice di un prodotto da eliminare: "))
+    listaspesa.pop(risp1)
     print(listaspesa)
 
-aggiunta()
-visualizza()
+while True:
+    print("1) aggiungi 2) visualizza 3) elimina ")
+    opzioni = int(input(""))
+    if opzioni == 1:
+        aggiunta()
+    elif opzioni == 2:
+        visualizza()
+    elif opzioni == 3:
+        elimina()  
+    break  
