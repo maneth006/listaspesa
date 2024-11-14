50e0083a92dd080b7ffb98a9da4af6e956925295
net <net@PC1-17.igalvani.local>
2024-11-14 09:30:12 +0100

    secondo commit

--- listaspesa.py
+++ listaspesa.py
@@ -2,6 +2,9 @@ listaspesa = []
 def aggiunta():
     risp = input("Dimmi un prodotto da aggiungere: ")
     listaspesa.append(risp)
+
+def visualizza():
     print(listaspesa)
 
 aggiunta()
+visualizza()
